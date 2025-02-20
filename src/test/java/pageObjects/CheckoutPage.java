6a8db8bd9acb9b39db993820a227b984195d0fa8
unknown <jellamanasa.99@gmail.com>
2025-02-20 16:42:04 -0500

    Updated page objects and added Testing.txt

--- src/test/java/pageObjects/CheckoutPage.java
+++ src/test/java/pageObjects/CheckoutPage.java
@@ -5,13 +5,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 public class CheckoutPage extends BasePage {
 
